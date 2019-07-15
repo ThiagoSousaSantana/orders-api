51fef82b2dbd126cf042954e13e88e3650b5b5b8
Thiago Sousa <thiago91098@gmail.com>
2019-07-14 22:23:42 -0300

    More swagger configuration

--- src/main/java/br/com/thiagosousa/ordersapi/config/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/thiagosousa/ordersapi/config/swagger/SwaggerConfiguration.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -16,6 +20,17 @@ public class SwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.thiagosousa.ordersapi"))
                 .paths(PathSelectors.ant("/**"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Orders API",
+                "A demo webservice for customers and orders management.",
+                "0.1",
+                "Terms of service",
+                new Contact("Thiago Sousa", "https://github.com/ThiagoSousaSantana/", "thiago91098@gmail.com"),
+                "License of API", "API license URL", Collections.emptyList());
     }
 }
